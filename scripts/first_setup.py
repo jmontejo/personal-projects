88a372e2890d3e28f79566b83816c789a6139084
jmontejo <jmontejo@4525493e-7705-40b1-a816-d608a930855b>
2015-09-07 21:43:26 +0000

    Improved first_setup, add my_vimrc
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlas-jmontejo/projects@10 4525493e-7705-40b1-a816-d608a930855b

--- scripts/first_setup.py
+++ scripts/first_setup.py
@@ -5,36 +5,44 @@ import sys,os
 if __name__ == "__main__":
 
   parser = OptionParser() 
-  #parser.add_option ("-L","--local", 
-  #                   help="Set up all machines respect to the local one",
-  #                   dest="all", 
-  #                   action="store_true", 
-  #                   default=False) 
+  parser.add_option ("-c","--clean", 
+                     help="Remove old files after setting up",
+                     dest="clean", 
+                     action="store_true", 
+                     default=False) 
+
   options,args = parser.parse_args()
   if len(args) != 1:
     print "Path to $MYSVN needed: first_setup.py $MYSVN"
     sys.exit(1)
   mysvn = args[0]+"/"
 
-  print "-- Link .bashrc .bash_profile"
-  if os.path.exists(os.path.expanduser("~/.bashrc")):
-    os.system("mv ~/.bashrc ~/.bashrc.old")
-    print "Previous .bashrc moved to .bashrc.old"
-  os.system("ln -s "+mysvn+"/scripts/my_bashrc ~/.bashrc")
-  if os.path.exists(os.path.expanduser("~/.bash_profile")):
-    os.system("mv ~/.bash_profile ~/.bash_profile.old")
-    print "Previous .bash_profile moved to .bash_profile.old"
-  os.system("ln -s "+mysvn+"/scripts/my_bash_profile ~/.bash_profile")
+  #original:(new,command to run before, command to run after)
+  files = {
+    "~/.bashrc"                   :("/scripts/my_bashrc",),
+    "~/.bash_profile"             :("/scripts/my_bash_profile",),
+    "~/.subversion/config"        :("/scripts/my_svnconfig.edit",
+                                        "sed  's/$MYSVN/%s/g' %s/scripts/my_svnconfig > %s/scripts/my_svnconfig.edit" % (mysvn.replace("/","\\/"),mysvn,mysvn) ),
+    "~/.vim/colors/mymorning.vim" :("/scripts/my_morning.vim",
+                                        "mkdir -p ~/.vim/colors"),
+    "~/.vimrc"                    :("/scripts/my_vimrc",),
+  }
 
-  print "-- Link .subversion/config"
-  if os.path.exists(os.path.expanduser("~/.subversion/config")):
-    os.system("mv ~/.subversion/config ~/.subversion/config.old")
-    print "Previous .subversion/config moved to .subversion/config.old"
-  os.system("ln -s "+mysvn+"/scripts/my_svnconfig ~/.subversion/config")
 
-  print "-- Link .vim/colors/mymorning.vim"
-  if os.path.exists(os.path.expanduser("~/.vim/colors/mymorning.vim")):
-    os.system("mv ~/.vim/colors/mymorning.vim ~/.vim/colors/mymorning.vim.old")
-    print "Previous .vim/colors/mymorning.vim moved to .vim/colors/mymorning.vim.old"
-  os.system("mkdir -p ~/.vim/colors")
-  os.system("ln -s "+mysvn+"/scripts/my_morning.vim ~/.vim/colors/mymorning.vim")
+  for old, new_ in files.iteritems():
+    new = mysvn+new_[0]
+
+    print "-- "+old
+    if os.path.exists(os.path.expanduser(old)):
+      old_bak = old+".old"
+      print "Previous %s moved to %s" % (old, old_bak)
+      os.system("mv %s %s" %(old,old_bak))
+      if options.clean:
+        os.system("rm %s" % old_bak)
+    if len(new_)>1:
+      precmd = new_[1]
+      os.system(precmd)
+    os.system("ln -s %s %s" % (new,old))
+    if len(new_)>2:
+      postcmd = new_[2]
+      os.system(postcmd)
