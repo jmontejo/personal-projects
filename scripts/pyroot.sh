02e9094fcaa62b81725b952552d68a5a9097eb05
Javier Montejo <jmontejo@cern.ch>
2020-06-19 14:47:20 +0200

    up

--- scripts/pyroot.sh
+++ scripts/pyroot.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+#Open python in interactive session, import ROOT and open optional file
+
 python -i -c "
 print 'import ROOT, sys, os'
 import ROOT, sys, os
 if len(sys.argv)>1:
     rfile = ROOT.TFile.Open(sys.argv[1])
-    print 'rfile=',rfile
-" $1
+    if not rfile:
+        print 'Could not find file:',sys.argv[1]
+    else:
+        print 'rfile=',rfile
+" $1 #optional file
 
